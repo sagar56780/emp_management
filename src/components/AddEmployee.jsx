9ebe74b4851fcaaa1440700ed192602762642012
sagar kumar <81793637+sagar56780@users.noreply.github.com>
2024-06-06 15:40:29 +0530

    json-server installed and form created

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const AddEmployee = () => {
+const AddEmployee = () => {    
+ 
   return (
-    <div>AddEmployee</div>
+
+    <main>
+ <div className="form-container">
+    <h2>Employee Registration</h2>
+    <form id="employeeForm" action='http://localhost:4000/employee' method='post'>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" required/>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" required/>
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" required/>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+    
+    
+    </main>
   )
 }
 
